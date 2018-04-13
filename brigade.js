af1db2ae76764486a77cef86200e8a9219cbdcc1
Ian Henry <ihenry@chef.io>
2018-04-13 15:36:17 -0400

    corrected event name
    Signed-off-by: Ian Henry <ihenry@chef.io>

--- brigade.js
+++ brigade.js
@@ -1,6 +1,6 @@
 const { events } = require('brigadier')
 
-events.on("pkg_update", (brigadeEvent, project) => {
+events.on("pkg_check", (brigadeEvent, project) => {
   console.log("Checking for package updates!")
   var node = new Job("package_updater")
   node.image = "node:alpine"
